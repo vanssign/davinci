5f1662a7a37750f194055a1cb339726d5135cc38
vanssign <thindvansh1999@gmail.com>
2021-08-10 12:32:51 +0530

    env variables

--- config/fire-config.js
+++ config/fire-config.js
@@ -3,13 +3,13 @@ import firebase from 'firebase';
 //firebase configs const
 
 const firebaseConfig = {
-    apiKey: `${process.env.FIREBASE_API_KEY}`,
-    authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-    projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-    storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-    messagingSenderId: `${process.env.FIREBASE_MESSAGE_SENDER_ID}`,
-    appId: `${process.env.FIREBASE_APP_ID}`,
-    measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`
+    apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
+    authDomain: `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}`,
+    projectId: `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
+    storageBucket: `${process.env.NEXT_PUBLIC_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID}`,
+    appId: `${process.env.NEXT_PUBLIC_APP_ID}`,
+    measurementId: `${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`
 };
 
 try {
